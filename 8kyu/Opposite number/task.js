82a2d98c187da52dc38bdbae3c92deb6a680bc1d
Nobls <sergikon92@gmail.com>
2021-10-12 23:04:01 +0300

    solutions

---
+++ 8kyu/Opposite number/task.js
@@ -0,0 +1,20 @@
+// Very simple, given a number, find its opposite.
+
+//   Examples:
+
+//     1: -1
+//     14: -14
+//     -34: 34
+
+
+function opposite(number) {
+  return (-number)
+}
+
+// Test
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(opposite(1), -1,)
+//   });
+// });
