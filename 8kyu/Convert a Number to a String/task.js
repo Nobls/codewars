b0e8f4619edc0ff6f7a7a364b9d163bc35885eff
Nobls <sergikon92@gmail.com>
2021-10-12 22:59:17 +0300

    solutions

---
+++ 8kyu/Convert a Number to a String/task.js
@@ -0,0 +1,21 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know ?
+
+//   Examples :
+//   123 -- > "123"
+//   999 -- > "999"
+
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num)
+}
+
+// Test
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(numberToString(67), '67');
+//   });
+// });
