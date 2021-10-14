147dfd34cda6075701b46d115aa8387af2b10ce0
Nobls <sergikon92@gmail.com>
2021-10-14 21:47:21 +0300

    solutions

---
+++ 8kyu/Find the smallest integer in the array/tsak.js
@@ -0,0 +1,36 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given[34, 15, 88, 2] your solution will return 2
+// Given[34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let min = args[0]
+    for (let i = 0; i < args.length; i++) {
+      if (args[i] < min) {
+        min = args[i]
+      }
+    }
+    return min
+  }
+}
+
+// Test
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+
+// describe("Smallest Integer Tests", () => {
+//   let sif = new SmallestIntegerFinder();
+//   it("Fixed Tests", () => {
+//     assert.strictEqual(sif.findSmallestInt([78, 56, 232, 12, 8]), 8, 'Should return the smallest int 8');
+//     assert.strictEqual(sif.findSmallestInt([78, 56, 232, 12, 18]), 12, 'Should return the smallest int 12');
+//     assert.strictEqual(sif.findSmallestInt([78, 56, 232, 412, 228]), 56, 'Should return the smallest int 56');
+//     assert.strictEqual(sif.findSmallestInt([78, 56, 232, 12, 0]), 0, 'Should return the smallest int 0');
+//     assert.strictEqual(sif.findSmallestInt([1, 56, 232, 12, 8]), 1, 'Should return the smallest int 1');
+//   });
+// })
