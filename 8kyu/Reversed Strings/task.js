7be63b6e6e59c07db67be9c901275c23c7c33de2
Nobls <sergikon92@gmail.com>
2021-10-15 16:39:30 +0300

    solutions

---
+++ 8kyu/Reversed Strings/task.js
@@ -0,0 +1,23 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  => 'dlrow'
+
+
+function solution(str) {
+  return str.split('').reverse().join('')
+}
+
+// Test
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(solution('world'), 'dlrow');
+//     assert.strictEqual(solution('hello'), 'olleh');
+//     assert.strictEqual(solution(''), '');
+//     assert.strictEqual(solution('h'), 'h');
+//   });
+// });
