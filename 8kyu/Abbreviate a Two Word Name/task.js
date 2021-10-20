e95ae29b123be898329698fd5bf7aaa5306ff212
Nobls <sergikon92@gmail.com>
2021-10-20 18:07:59 +0300

    solutions

---
+++ 8kyu/Abbreviate a Two Word Name/task.js
@@ -0,0 +1,31 @@
+// Write a function to convert a name into initials.This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+
+function abbrevName(name) {
+  let nam = name.split(' ');
+  return nam[0].charAt(0).toUpperCase() + '.' + nam[1].charAt(0).toUpperCase();
+}
+
+// Test
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+//     assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+//     assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+//     assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+//     assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+//   });
+// });
