d03e750e65f7c04cbea4b684f3dc9f7625cc2f2c
Nobls <sergikon92@gmail.com>
2021-10-22 18:31:17 +0300

    solutions

---
+++ 8kyu/MakeUpperCase/task.js
@@ -0,0 +1,13 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// Test
+
+// describe("Basic Tests", function () {
+//   it("should pass the basic tests", function () {
+//     Test.assertEquals(makeUpperCase("hello"), "HELLO");
+//   });
+// });
